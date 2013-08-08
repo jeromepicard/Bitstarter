54bfd50103fa94fe5e0e5c67ad889c6819084e76
Jerome Picard <jerome.picard@gmail.com>
2013-08-08 15:10:22 +0200

    Added index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = new Buffer;
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+  response.send(data.toString);
 });
 
 var port = process.env.PORT || 5000;
